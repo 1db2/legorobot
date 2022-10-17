70ee41e26af4286196395aa0a26cbc90e073d460
idb <86549645+1db2@users.noreply.github.com>
2022-10-17 10:14:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import math
 
 ev3 = EV3Brick()
 
+
 def wait_for_button(ev3):
     pressed = []
 
@@ -26,6 +27,7 @@ def wait_for_button(ev3):
 
     return button
 
+
 x = 178/2
 y = 128/2
 r = 10
@@ -36,6 +38,7 @@ ingame = True
 
 circles = []
 
+
 class Circle:
     def __init__(self, x, y, r, fill):
         self.x = x
@@ -45,23 +48,27 @@ class Circle:
         self.fill = fill
         self.color = Color.BLACK
 
+
 player = Circle(x, y, r, True)
 
+
 def RandomCircle():
-    rR = rand.randint(player.r-8, player.r+2)
+    rR = rand.randint(player.r-8, player.r+5)
     rX = rand.randint(0+rR, 178-rR)
     rY = rand.randint(0+rR, 128-rR)
 
     rCircle = Circle(rX, rY, rR, False)
 
     return rCircle
-  
-def Distance(c1,c2):
-  return math.sqrt( ((c2.x+c1.x)**2) + ((c2.y+c1.y)**2) )
+
+
+score = 0
 
 
 while ingame:
-    ev3.screen.draw_circle(player.x, player.y, player.r, player.fill, player.color)
+    ev3.screen.draw_text(0, 0, "Score: " + str(score), Color.BLACK, None)
+    ev3.screen.draw_circle(player.x, player.y, player.r,
+                           player.fill, player.color)
 
     if len(circles) < 5:
         for _ in range(5 - len(circles)):
@@ -76,8 +83,11 @@ while ingame:
             if circle.r <= player.r:
                 circles.remove(circle)
                 player.r = player.r + circle.r/4
+                score = round(player.r)
+                print(score)
+                ev3.speaker.beep()
             elif circle.r > player.r:
-                print('vege')
+                ingame = False
                 break
 
     button = wait_for_button(ev3)
@@ -92,3 +102,8 @@ while ingame:
         player.y = player.y + 10
 
     ev3.screen.clear()
+
+ev3.screen.clear()
+ev3.screen.print("Game over\n")
+ev3.screen.print("Final score: " + str(score))
+wait_for_button(ev3)
